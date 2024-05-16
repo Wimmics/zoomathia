afcca38199ddbb6260524e5919e095604129d904
dhouib <molkatounsi@gmail.com>
2024-05-16 16:45:09 +0200

    add tlg0024 and tlg0057 Dbpedia extraction

--- Named_entity_recognition/NER_main.py
+++ Named_entity_recognition/NER_main.py
@@ -5,7 +5,7 @@ from Named_entity_recognition.translation_to_csv_step import main_transformation
 
 # Obtenez le chemin du répertoire du script
 repertoire_script = os.path.dirname(os.path.abspath(__file__))
-nom_dossier = "tlg0032/tlg014"
+nom_dossier = "tlg0024/tlg001"
 
 # path for the original data
 original_data = os.path.join(repertoire_script, "data/original_files/" + nom_dossier)
