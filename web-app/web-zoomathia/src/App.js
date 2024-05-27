174604de46adbcb91edd63a000d05c5f6c28418c
Sensaku <barbe.arnaud@hotmail.fr>
2024-05-27 11:27:40 +0200

    Start Book display by a given uri implementation

--- web-app/web-zoomathia/src/App.js
+++ web-app/web-zoomathia/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/page/Navbar';
 import Footer from './components/page/Footer';
 import Home from './components/page/Home';
 import SparqlTest from './components/SparqlTest';
+import Work from './components/WorkComponent';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CompetencyQuestionComponent from './components/CompetencyQuestionComponent';
 
@@ -19,6 +20,7 @@ function App() {
         <Route path='ExploreTheCorpus' element={<SearchPage />} />
         <Route path='CompetencyQuestion' element={<CompetencyQuestionComponent />} />
         <Route path='SPARQLJS' element={< SparqlTest />} />
+        <Route path='Work' element={<Work />} />
       </Routes>
       <Footer />
     </BrowserRouter>
