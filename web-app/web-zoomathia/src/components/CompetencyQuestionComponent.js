59eb70e065c701f044d594611ec5ddb42fc65eb4
Sensaku <barbe.arnaud@hotmail.fr>
2024-05-29 11:47:03 +0200

    Add clickable link to paragraph SPARQL variable

--- web-app/web-zoomathia/src/components/CompetencyQuestionComponent.js
+++ web-app/web-zoomathia/src/components/CompetencyQuestionComponent.js
@@ -2,19 +2,28 @@ import { useState, useEffect, useCallback } from "react"
 import styles from "./css_modules/CompetencyQuestionComponent.module.css"
 import "gridjs/dist/theme/mermaid.min.css";
 import { Grid } from "gridjs-react"
+import { html } from 'gridjs'
 import Select from 'react-select'
 
 const CompetencyQuestionComponent = () => {
+    const styleGrid = {
+        td: {
+            'text-overflow': 'ellipsis',
+            'overflow': 'hidden',
+            'white-space': 'normal'
+        }
+    }
+
     const [options, setOptions] = useState([])
-    const [data, setData] = useState([])
-    const [columns, setColumns] = useState([])
     const [title, setTitle] = useState("")
     const [iframe, setIframe] = useState(<></>)
+    const [table, setTable] = useState(<Grid data={[]} columns={[]} pagination={{ limit: 10 }} search={true} style={styleGrid} />)
 
     const updateTable = useCallback((e) => {
         const file = e.value
         const title = e.label
         const callForData = async () => {
+            const generatedCol = []
             if (file === null) { return }
             const styleSheet = `{
                 "appli": {
@@ -63,8 +72,18 @@ const CompetencyQuestionComponent = () => {
             }`
             const data = await fetch(`${process.env.REACT_APP_BACKEND_URL}getQC?id=${file}`).then(response => response.json())
             setTitle(title)
-            setColumns(data.table.columns)
-            setData(data.table.data)
+            for (const elt of data.table.columns) {
+                if (elt === "paragraph") {
+                    generatedCol.push({
+                        name: elt,
+                        formatter: (cell) => { return html(`<a href='${process.env.REACT_APP_FRONTEND_URL}Work?uri=${cell}' target='_blank'>${cell}</a>`) }
+                    })
+                } else {
+                    generatedCol.push(elt)
+                }
+            }
+            console.log(html)
+            setTable(<Grid data={data.table.data} columns={generatedCol} pagination={{ limit: 10 }} search={true} style={styleGrid} />)
             setIframe(<iframe className={styles["iframe-box"]}
                 title="Query visualisation"
                 src={`${process.env.REACT_APP_LDVIZ_URL}ldviz?url=${process.env.REACT_APP_CORESE_URL}&query=${encodeURIComponent(data.spo)}&stylesheet=${encodeURIComponent(styleSheet)}`}
@@ -72,14 +91,13 @@ const CompetencyQuestionComponent = () => {
             </iframe>)
         }
         callForData()
-    }, [setData, setColumns, setIframe])
+    }, [setTable, setIframe])
 
     useEffect(() => {
         const callForData = async () => {
             const data = await fetch(`${process.env.REACT_APP_BACKEND_URL}qcList`).then(response => response.json())
             const optionsList = []
             for (const row of data) {
-                console.log(row.id, row.file)
                 optionsList.push({ value: row.id, label: row.title })
             }
             setOptions(optionsList)
@@ -92,7 +110,7 @@ const CompetencyQuestionComponent = () => {
         <h2 key="titre_competence">Select a competency question</h2>
         <Select onChange={updateTable} options={options} />
         <h3>{title}</h3>
-        <Grid data={data} columns={columns} pagination={{ limit: 10 }} search={true} />
+        {table}
         <h3>Visualisation</h3>
         {iframe}
     </div>
