d6d3e5e5e6f36fe1ae3fe520873fe8ea71d61680
Sensaku <barbe.arnaud@hotmail.fr>
2024-07-11 11:21:03 +0200

    Fix typo on title

--- web-app/web-zoomathia/src/components/page/Navbar.js
+++ web-app/web-zoomathia/src/components/page/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
                 <NavLink to='/' className={({ isActive }) => isActive ? 'active' : ''}>Home</NavLink>
                 <NavLink to='/ExploreTheCorpus' className={({ isActive }) => isActive ? 'active' : ''}>Explore the corpus</NavLink>
                 <NavLink to='/ExploreAWork' className={({ isActive }) => isActive ? 'active' : ''}>Explore a work</NavLink>
-                <NavLink to='/CompetencyQuestion' className={({ isActive }) => isActive ? 'active' : ''}>Competency question</NavLink>
+                <NavLink to='/CompetencyQuestion' className={({ isActive }) => isActive ? 'active' : ''}>Competency questions</NavLink>
             </div>
         </nav>
 
