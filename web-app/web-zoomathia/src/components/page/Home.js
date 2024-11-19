de322378d1cced695921d7bed3d46f44275d2e43
Sensaku <barbe.arnaud@hotmail.fr>
2024-11-19 16:32:52 +0100

    Update Home page

--- web-app/web-zoomathia/src/components/page/Home.js
+++ web-app/web-zoomathia/src/components/page/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
             <p>This application is being developed within the framework of the <a target="_blank" rel="noreferrer" href="https://univ-cotedazur.fr/recherche-innovation/structures-de-recherche/academies-dexcellence/academie-dexcellence-homme-idees-et-milieux/projets-de-recherche/projets-2020-2024/hisinum">HisINum project</a> funded by the <a href="https://univ-cotedazur.fr/recherche-innovation/structures-de-recherche/academies-dexcellence/academie-dexcellence-homme-idees-et-milieux" rel="noreferrer" target="_blank">Academy of Excellence 5</a> of <a href="https://univ-cotedazur.fr/recherche-innovation/defis-scientifiques-idex" rel="noreferrer" target="_blank">IdEx UCA JEDI</a>.</p>
             <p>It aims to support the study of the transmission of zoological knowledge
                 from antiquity to the Middle Ages through the analysis of a corpus of
-                texts on animals compiled within the <a href="https://www.cepam.cnrs.fr/sites/zoomathia/presentation-generale-du-gdri-zoomathia/" target="_blank" rel="noreferrer" >Zoomathia GDRI</a> funded by the CNRS.<br /><br /></p>
+                texts on animals compiled within the framework of the <a href="https://www.cepam.cnrs.fr/sites/zoomathia/presentation-generale-du-gdri-zoomathia/" target="_blank" rel="noreferrer" >Zoomathia GDRI</a> funded by the CNRS.<br /><br /></p>
             <p>It allows:</p>
             <ul className={styles["list-items"]}>
                 <li>exploration of the corpus, via a search for works by concept;</li>
