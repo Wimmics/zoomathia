dc2d5c1128babe25a5bf3b529dedbaeb695ba3f2
Sensaku <barbe.arnaud@hotmail.fr>
2024-11-20 17:29:02 +0100

    Fix CSS issues

--- web-app/web-zoomathia/src/components/page/Home.js
+++ web-app/web-zoomathia/src/components/page/Home.js
@@ -16,14 +16,14 @@ const Home = () => {
                 competency questions on a selected work from the corpus.</li>
             </ul>
 
-            <p>It relies on the exploitation of a knowledge graph annotating the
+            <p><br />It relies on the exploitation of a knowledge graph annotating the
             Zoomathia corpus of texts with concepts from the <a href="https://opentheso.huma-num.fr/opentheso/?idt=th310" rel="noreferrer" target="_blank">TheZoo thesaurus</a>.</p>
-            <p>The pipeline for the automatic construction of this knowledge graph was
+            <p><br />The pipeline for the automatic construction of this knowledge graph was
             developed within the framework of the <a href="https://univ-cotedazur.fr/recherche-innovation/structures-de-recherche/academies-dexcellence/academie-dexcellence-homme-idees-et-milieux/projets-de-recherche/projets-2020-2024/automazoo-annotation-automatique-dun-corpus-zoologique-ancien" rel="noreferrer" target="_blank">AutomaZoo project</a> funded by the
             Academy of Excellence 5 of IdEx UCA JEDI, and further refined within the
             framework of the HisINum project.</p>
-            <p>GitHub of the project: <a href="https://github.com/Wimmics/zoomathia" rel="noreferrer" target="_blank">https://github.com/Wimmics/zoomathia</a></p>
-            <p>Access to the knowledge graph through its SPARQL endpoint:
+            <p><br />GitHub of the project: <a href="https://github.com/Wimmics/zoomathia" rel="noreferrer" target="_blank">https://github.com/Wimmics/zoomathia</a></p>
+            <p><br />Access to the knowledge graph through its SPARQL endpoint:
             <a href="http://zoomathia.i3s.unice.fr/sparql" rel="noreferrer" target="_blank">http://zoomathia.i3s.unice.fr/sparql</a><br /><br /><br /></p>
             <p>Contact: <a href="mailto:faro@i3s.unice.fr">Catherine Faron</a></p>
             
